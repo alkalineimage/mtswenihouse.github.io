31adac53144810b3c4502e1cc62cda1f6dae0ba3
mh <alkalineimage@gmail.com>
2026-04-05 13:56:42 +0200

    Main Code Upkeep

---
+++ mhpack/00roots/00roots01text.js
@@ -0,0 +1,18 @@
+
+
+const 	famtexts = [{
+		id:0,	name01:"Mtsweni"	,
+				name02:"House"		,
+				name03:"since '10"		,
+	}]
+const	famtext =	famtexts[0];
+
+const 	familymembers = [{
+		id:0,	name00:famtext.name01+famtext.name02, nickname00:"MH"			, birth00:"2010.12.10", starsign00:"Milkyway"	, spiranimal00:"Monkey",		
+		id:1,	name01:"Skhumbuzo"					, nickname01:"tHR13tEen"	, birth01:"1984.01.17", starsign01:"Capricorn"	, spiranimal01:"Goat",			
+		id:2,	name02:"Bongiwe"					, nickname02:"Kiwi"			, birth02:"1989.04.13", starsign02:"Aries"		, spiranimal02:"Owl",			
+		id:3,	name03:"Khayelihle"					, nickname03:"Khayo"		, birth03:"2012.01.10", starsign03:"Capricorn"	, spiranimal03:"Bear",			
+		id:4,	name04:"Siphesihle"					, nickname04:"Wednesday"	, birth04:"2013.01.25", starsign04:"Aquarius"	, spiranimal04:"Bat",		
+		id:5,	name05:"Wandile"					, nickname05:"Wawi"			, birth05:"2017.01.20", starsign05:"Aquarius"	, spiranimal05:"Tarantula",		
+	}]
+const	fam00member =	familymembers[0];
